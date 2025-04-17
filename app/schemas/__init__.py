0f4a72b354108c2c16c99f7b4077d07581d5d365
Rajan Neupane <rajanneupane355@example.com>
2025-04-17 14:01:37 +0530

    init.py committed

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,7 @@
 from app.schemas.user import User, UserCreate, UserUpdate, UserInDB, Token, TokenData
 from app.schemas.flight import Flight, FlightCreate, FlightUpdate, FlightSearch
 from app.schemas.booking import (
-    BOoking,
+    Booking,
     BookingCreate,
     BookingUpdate,
     PaymentCreate,
